5baade24ca8540d10f6d64658e41624892ac7350
ramyaa2520 <154006313+ramyaa2520@users.noreply.github.com>
2025-04-03 23:01:59 +0530

    Update page.jsx

--- src/app/landing-page/page.jsx
+++ src/app/landing-page/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavigationBar from "../../components/navigation-bar";
 
 function MainComponent() {
